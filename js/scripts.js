d15526bf115241cef65ad80772c692f468cc37e0
Esmeralda Gonzalez <neesgonzalezno@ittepic.edu.mx>
2026-07-27 19:15:26 -0700

    Delete js/scripts.js

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function menuBarras(){
-    const iconoBarras = document.querySelector(".icono-barras");
-    const navegacion = document.querySelector(".navegacion-principal");
-    iconoBarras.addEventListener("click", (e) =>{
-        navegacion.classList.toggle("activo");
-    })
-}
-
-menuBarras();
